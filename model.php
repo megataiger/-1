d46470f9c275e913f9d545ff5ca6be6c7bdd06c7
megataiger <45592104+megataiger@users.noreply.github.com>
2019-03-20 17:12:25 +0400

    Update model.php

--- model.php
+++ model.php
@@ -1,20 +1,29 @@
  <?php
+
 $link = mysqli_connect("127.0.0.1","megataiger","","base");
 
+if (!$link)
+{
+	$menu = "Упс!";
+	$body = "Упс!";
+}
+
+else
+{
 $zap = mysqli_query($link, 'select * from CMS order by page_weight asc');
 
 $menu = "\t<ul>\n";
 
 while($result = mysqli_fetch_assoc($zap))
 {
+	if (isset($_GET["temp"]))
+	{ 
 	$stroke = $result;
 	$spisok = array(
 		"page_id" => $stroke["page_id"], 
 		"page_name" =>  $stroke["page_name"],
 		"page_text" => $stroke["page_text"]
 					);
-	if (isset($_GET["temp"]))
-	{ 
 		if($_GET["temp"] == $spisok["page_id"])
 		{	
 			$menu .= "\t\t<li>" . $spisok["page_name"] . "</li>\n";
@@ -25,11 +34,18 @@ while($result = mysqli_fetch_assoc($zap))
 	}
 	else
 	{
+		$stroke = $result;
+		$spisok = array(
+			"page_id" => $stroke["page_id"], 
+			"page_name" =>  $stroke["page_name"],
+					);
 		$menu .= "\t\t<li><a href=?temp=" . $spisok["page_id"] . ">" . $spisok["page_name"]."</a></li>\n";
 		$body = "Добро пожаловать!\n";
 	}
 }
 
 $menu .= "\t</ul>\n";
+}
+mysqli_close($link);
 
  ?>
